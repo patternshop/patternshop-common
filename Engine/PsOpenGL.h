1882bb5a0e71b850ee5f6d5dea16ccda0e1aa7cd
Antoine Viellard <antoine.viellard@gmail.com>
2024-07-02 20:07:50 +0100

    Formatting

---
+++ Engine/PsOpenGL.h
@@ -0,0 +1,30 @@
+/**
+ * This file is part of Patternshop Project.
+ *
+ * Patternshop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Patternshop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Patternshop.  If not, see <http://www.gnu.org/licenses/>
+*/
+#ifndef PS_OPENGL_H__
+
+#define PS_OPENGL_H__
+
+#ifdef _MACOSX
+# include <OpenGL/gl.h>
+# include <OpenGL/glu.h>
+#else
+# include "stdafx.h"
+# include <GL/gl.h>
+# include <GL/glu.h>
+#endif
+
+#endif
